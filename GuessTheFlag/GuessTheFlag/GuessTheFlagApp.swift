ee160e79a0e8d71b43c4b551ddcc13808231e526
Damagucci-Juice <jsr9874@gmail.com>
2022-10-08 15:36:30 +0900

    init: GuessTheFlag Project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by YEONGJIN JANG on 2022/10/08.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
