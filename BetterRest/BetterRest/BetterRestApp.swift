91a32212350acb3ab8f95d2fb6ed7f5ad6534fd5
Damagucci-Juice <jsr9874@gmail.com>
2022-10-18 23:05:48 +0900

    feat: Better Rest 구현 완료

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by YEONGJIN JANG on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
