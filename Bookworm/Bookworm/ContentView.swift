927b36a34aad8fbde616dd7ee1d4d6349a9cf75a
Damagucci-Juice <jsr9874@gmail.com>
2022-11-29 16:27:45 +0900

    Day54: CustomView 연결 완료

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by YEONGJIN JANG on 2022/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        Text(book.title ?? "N/A")
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
