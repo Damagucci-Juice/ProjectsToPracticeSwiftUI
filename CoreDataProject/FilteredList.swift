5f849f8deeef7020614dc8bbb554c6110283b51a
Damagucci-Juice <jsr9874@gmail.com>
2023-06-03 11:22:01 +0900

    day58: FilteredList 완성

---
+++ CoreDataProject/FilteredList.swift
@@ -0,0 +1,28 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Gucci on 2023/06/03.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    @FetchRequest var fetchRequest: FetchedResults<Singer>
+    
+    init(filter: String) {
+        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    }
+    
+    var body: some View {
+        List(fetchRequest, id: \.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+}
+
+struct FilteredList_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteredList(filter: "A")
+    }
+}
---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Gucci on 2023/06/03.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
