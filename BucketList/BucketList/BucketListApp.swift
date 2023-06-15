8f14f4191147d841201254a72c942a17a7b673fd
Damagucci-Juice <jsr9874@gmail.com>
2023-06-15 20:30:43 +0900

    day68: change view status

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Gucci on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
