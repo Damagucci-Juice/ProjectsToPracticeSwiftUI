f9eaaff32dae275432d293df26673775b406d905
Damagucci-Juice <jsr9874@gmail.com>
2023-06-16 16:40:56 +0900

    day71: Downloading data from Wikipedia

---
+++ BucketList/BucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Gucci on 2023/06/16.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
