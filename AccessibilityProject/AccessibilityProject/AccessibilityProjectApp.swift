1fa79a46bae6b7d64d66d2e5eb536c4d7e87afde
Damagucci-Juice <jsr9874@gmail.com>
2023-06-17 15:41:28 +0900

    day74: Identifying views with useful labels

---
+++ AccessibilityProject/AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Gucci on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
