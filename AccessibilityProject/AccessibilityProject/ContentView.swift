8b44448778755ecb38fa5f89eb97df581f2fa207
Damagucci-Juice <jsr9874@gmail.com>
2023-06-17 16:04:23 +0900

    day74: Reading the value of controls

--- AccessibilityProject/AccessibilityProject/ContentView.swift
+++ AccessibilityProject/AccessibilityProject/ContentView.swift
@@ -8,34 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks",
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var value = 10
 
     var body: some View {
-        ZStack {
-            Image(decorative: pictures[selectedPicture])
-                .accessibilityHidden(true)
-            
-            VStack {
-                Text("Your score is")
-                Text("1000")
-                    .font(.title)
+        VStack {
+            Text("Value: \(value)")
+
+            Button("Increment") {
+                value += 1
             }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Your score is 1000")
+
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not Handled.")
+            }
         }
     }
 }
