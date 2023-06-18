800651fa36b1cbe26cd9e6a4e849cbc272b1ed85
Damagucci-Juice <jsr9874@gmail.com>
2023-06-18 18:56:21 +0900

    day81: Adding Swift package dependencies in Xcode

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,32 +6,18 @@
 //
 
 import SwiftUI
-import UserNotifications
+import SamplePackage
 
 struct ContentView: View {
+    let possibles = Array(1...60)
+    var results: String {
+        let selected = possibles.random(7).sorted()
+        let str = selected.map(String.init)
+        return str.joined(separator: ", ")
+    }
+    
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.sound,.alert,.badge]) { isSuccess, error in
-                    if isSuccess {
-                        print("Succes")
-                    } else if let error = error {
-                        print("Error: \(error.localizedDescription)")
-                    }
-                }
-            }
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.body = "It looks hungry"
-                content.sound = .default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text(results)
     }
 }
 
