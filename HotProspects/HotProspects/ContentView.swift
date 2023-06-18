ec50e0b8e9c30761ba0ef43559b112547e1e7929
Damagucci-Juice <jsr9874@gmail.com>
2023-06-18 19:07:25 +0900

    dya82: Building our tab bar

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -9,15 +9,25 @@ import SwiftUI
 import SamplePackage
 
 struct ContentView: View {
-    let possibles = Array(1...60)
-    var results: String {
-        let selected = possibles.random(7).sorted()
-        let str = selected.map(String.init)
-        return str.joined(separator: ", ")
-    }
-    
     var body: some View {
-        Text(results)
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
 }
 
