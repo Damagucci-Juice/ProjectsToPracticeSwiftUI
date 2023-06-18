df771b7de32960f23fa8727a5a319e7961f3fe8b
Damagucci-Juice <jsr9874@gmail.com>
2023-06-18 16:18:18 +0900

    day80: Understanding Swift’s Result type

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,31 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var updater = DelayedUpdater()
+    @State private var output = ""
     
     var body: some View {
-        Text("Value is: \(updater.value)")
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        let result = await fetchTask.result
+        
+//        //MARK: decapsulation ver 1
+//        do {
+//            output = try result.get()
+//        } catch {
+//            output = "Error: \(error.localizedDescription)"
+//        }
+        
+        //MARK: decapsulation ver 2
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-class DelayedUpdater: ObservableObject {
-    var value = 0 {
-        willSet {
-            objectWillChange.send()
-        }
-    }
-    
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now()+Double(i)) {
-                self.value += 1
-            }
-        }
     }
 }
