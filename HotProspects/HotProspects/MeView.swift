2b5466015f953a7ca8b5560d21a499a4c4a960c0
Damagucci-Juice <jsr9874@gmail.com>
2023-06-19 20:09:02 +0900

    day84: Adding a context menu to an image

--- HotProspects/HotProspects/MeView.swift
+++ HotProspects/HotProspects/MeView.swift
@@ -11,6 +11,7 @@ import CoreImage.CIFilterBuiltins
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.com"
+    @State private var qrcode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -26,14 +27,29 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrcode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrcode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
+    }
+    
+    func updateCode() {
+        qrcode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
@@ -41,7 +57,8 @@ struct MeView: View {
 
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgimg)
+                let image = UIImage(cgImage: cgimg)
+                return image
             }
         }
 
