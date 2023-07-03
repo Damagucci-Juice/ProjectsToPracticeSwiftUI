2d774e2aacf7e1bde2e5e7d13a99bc753a2f8c79
Damagucci-Juice <jsr9874@gmail.com>
2023-07-03 19:04:37 +0900

    day86: Making vibrations with UINotificationFeedbackGenerator and Core Haptics

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Gucci on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
