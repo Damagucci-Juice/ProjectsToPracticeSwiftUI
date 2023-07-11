c13a97f3ecb3f5349472bc2ec60ac8f545ba8704
Damagucci-Juice <jsr9874@gmail.com>
2023-07-11 20:16:19 +0900

    day92: intro

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Gucci on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
