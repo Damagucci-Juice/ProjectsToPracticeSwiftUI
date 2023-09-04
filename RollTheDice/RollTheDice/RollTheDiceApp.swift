5a11073e5042bcf8ea7430e3b90447579e591626
Damagucci-Juice <jsr9874@gmail.com>
2023-09-04 22:30:08 +0900

    init: Roll The dice

---
+++ RollTheDice/RollTheDice/RollTheDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceApp.swift
+//  RollTheDice
+//
+//  Created by Gucci on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
